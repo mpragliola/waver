45eeb2245ca48b2f9cbeb5fea58c7a7969495d44
Marco Pragliola <marcopragliola@gmail.com>
2026-08-01 07:17:34 +0200

    Export pixelToSample and sampleToPixel functions from core/viewport in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { WaverElement, defineWaverElement } from "./waver-element";
 export { darkTheme, lightTheme, resolveTheme, deriveSelectionColor } from "./core/theme";
 export { computePeaks, fullZoomSamplesPerPixel, MIN_SAMPLES_PER_PIXEL } from "./core/peaks";
+export { pixelToSample, sampleToPixel } from "./core/viewport";
 export * from "./core/types";
 
 declare global {
